17269ebd38a20752ceadb763cf75ed50b6c51f97
Octavian Ionescu <itavyg@gmail.com>
2019-01-27 21:07:59 +0200

    [dev] bump dev dependencies
    Signed-off-by: Octavian Ionescu <itavyg@gmail.com>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const semver = require('semver');
 
-const minNodeVersion = '6.0.0';
+const minNodeVersion = '8.6.0';
 
 /**
  * check if min nodejs requirements are met
@@ -12,10 +12,7 @@ const getVersionModule = () => {
   if (semver.lt(process.version, minNodeVersion)) {
     throw Error(`Invalid node version for @itavy/mq-structure, current: ${process.version}, min: ${minNodeVersion}`);
   }
-  if (semver.major(process.version) === semver.major(minNodeVersion)) {
-    // eslint-disable-next-line global-require
-    return require('./lib/v6x');
-  }
+
   // eslint-disable-next-line global-require
   return require('./lib/latest');
 };
